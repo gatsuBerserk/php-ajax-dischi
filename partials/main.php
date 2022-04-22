5d5b14321a772bc81a1351341481363fef653e34
gatsuBerserk <berserkdev@outlook.it>
2022-04-22 16:40:38 +0200

    mimleston 1- classic view disc- without api

--- partials/main.php
+++ partials/main.php
@@ -1,3 +1,30 @@
+<?php 
+    include_once __DIR__ ."/../server/data.php"; 
+?>
 <main>
-    <pre>Main</pre>
-</main>
+    <section> 
+        <?php foreach($discs as $disc) {?>
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                    <img src="<?= $disc['poster']; ?>" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title"><?= $disc['title']; ?></h5>
+                            <p class="card-text"><?= $disc['author']; ?></p>
+                            <p class="card-text"><?= $disc['year']; ?></p>
+                            <p class="card-text"><?= $disc['genre']; ?></p> 
+
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+       
+        <?php }?>
+    
+    </section>
+</main> 
+
+
--- server/data.php
+++ server/data.php
@@ -1,10 +1,74 @@
 <?php 
   $discs = [
-      [
-        "title"=>"titolo", 
-        "author" => "autore", 
-        "anno" => "anno", 
-        "genre" => "genere"
-      ],
+    [
+        "poster" => "https://m.media-amazon.com/images/I/81WCr-L10EL._AC_SX450_.jpg",
+        "title" => "Painkiller",
+        "author" => "Judas Priest",
+        "genre" => "Metal",
+        "year" => "1990",
+    ],
+    [
+        "poster" => "https://dreamtheater.net/wp-content/uploads/2017/07/dramaticturnofevents-800px-squashed.jpg",
+        "title" => "Breaking All Illusion",
+        "author" => "Dream Theater",
+        "genre" => "Metal",
+        "year" => "2011",
+    ],
+    [
+        "poster" => "https://www.virginradio.it/upload/1555062492859.jpeg",
+        "title" => "Blue On Black",
+        "author" => "Five Finger Death Punch ",
+        "genre" => "Metal",
+        "year" => "2019",
+    ],
+    [
+        "poster" => "https://m.media-amazon.com/images/I/81cWtLkZgxL._SS500_.jpg",
+        "title" => "La maledizione di Capitan Findus ",
+        "author" => "NanowaR of Steel",
+        "genre" => "Metal",
+        "year" => "2021",
+    ],
+    [
+        "poster" => "https://m.media-amazon.com/images/I/81WCr-L10EL._AC_SX450_.jpg",
+        "title" => "Painkiller",
+        "author" => "Judas Priest",
+        "genre" => "Metal",
+        "year" => "1990",
+    ],
+    [
+        "poster" => "https://m.media-amazon.com/images/I/81WCr-L10EL._AC_SX450_.jpg",
+        "title" => "Painkiller",
+        "author" => "Judas Priest",
+        "genre" => "Metal",
+        "year" => "1990",
+    ],
+    [
+        "poster" => "https://m.media-amazon.com/images/I/81WCr-L10EL._AC_SX450_.jpg",
+        "title" => "Painkiller",
+        "author" => "Judas Priest",
+        "genre" => "Metal",
+        "year" => "1990",
+    ],
+    [
+        "poster" => "https://m.media-amazon.com/images/I/81WCr-L10EL._AC_SX450_.jpg",
+        "title" => "Painkiller",
+        "author" => "Judas Priest",
+        "genre" => "Metal",
+        "year" => "1990",
+    ],
+    [
+        "poster" => "https://m.media-amazon.com/images/I/81WCr-L10EL._AC_SX450_.jpg",
+        "title" => "Painkiller",
+        "author" => "Judas Priest",
+        "genre" => "Metal",
+        "year" => "1990",
+    ],
+    [
+        "poster" => "https://m.media-amazon.com/images/I/81WCr-L10EL._AC_SX450_.jpg",
+        "title" => "Painkiller",
+        "author" => "Judas Priest",
+        "genre" => "Metal",
+        "year" => "1990",
+    ],
   ]
 ?> 
